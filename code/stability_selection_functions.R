d29307a5aea9fdcc9924cc8469df51419f9d6239
Annie Xie <xxie306@gmail.com>
2025-09-17 13:45:45 -0500

    Correct row-split stability selection and update similarity computation

--- code/stability_selection_functions.R
+++ code/stability_selection_functions.R
@@ -11,47 +11,85 @@ stability_selection_split_data <- function(X, dim = c('rows', 'columns')){
   } else {
     stop('Wrong input for dim')
   }
-  
+
   subset1 <- sample(n, size = n1, replace = FALSE)
-  X1 <- X[,subset1]
-  X2 <- X[,-(subset1)]
-  return(list(X1, X2))
+  if (dim == 'columns'){
+    X1 <- X[,subset1]
+    X2 <- X[,-(subset1)]
+  } else { # dim = 'rows'
+    X1 <- X[subset1,]
+    X2 <- X[-(subset1),]
+  }
+  return(list(X1, X2, subset1, setdiff(c(1:n), subset1)))
+}
+
+compute_cosine_sim_matrix <- function(L1, L2){
+  norms1 <- apply(L1, 2, function(x){sqrt(sum(x^2))})
+  norms1[norms1 == 0] <- Inf
+
+  norms2 <- apply(L2, 2, function(x){sqrt(sum(x^2))})
+  norms2[norms2 == 0] <- Inf
+
+  L1_normalized <- t(t(L1)/norms1)
+  L2_normalized <- t(t(L2)/norms2)
+
+  #compute matrix of cosine similarities
+  cosine_sim_matrix <- crossprod(L1_normalized, L2_normalized)
+
+  return(cosine_sim_matrix)
 }
 
 stability_selection_post_processing <- function(L1, L2, threshold=0.99){
   # use cosine similarity as similarity metric
   # could try correlation but may run into issues if there is a constant baseline
-  norms1 <- apply(L1, 2, function(x){sqrt(sum(x^2))})
-  norms1[norms1 == 0] <- Inf
-  
-  norms2 <- apply(L2, 2, function(x){sqrt(sum(x^2))})
-  norms2[norms2 == 0] <- Inf
-  
-  L1_normalized <- t(t(L1)/norms1)
-  L2_normalized <- t(t(L2)/norms2)
-  
-  #compute matrix of cosine similarities
-  cosine_sim_matrix <- abs(crossprod(L1_normalized, L2_normalized))
-  
+
+  # compute cosine similarity
+  cosine_sim_matrix <- compute_cosine_sim_matrix(L1, L2)
+
   #compute maximum similarity for each factor
   idx <- which.min(dim(cosine_sim_matrix))
-  
+
   #pick factors to keep
   max_similarity <- apply(cosine_sim_matrix, idx, max)
   factors.idx.keep <- which(max_similarity > threshold)
   if (length(factors.idx.keep) == 0){
-    return(list(L = matrix(0, nrow = nrow(L1), ncol = 1), 
-                similarity = cosine_sim_matrix))
+    L_final = matrix(0, nrow = nrow(L1), ncol = 1)
+  } else if (idx == 1) {
+    L_final <- L1[,factors.idx.keep, drop = FALSE]
+  } else { # captures idx == 2
+    L_final <- L2[, factors.idx.keep, drop = FALSE]
   }
-  # I think this could lead to redundant factors?
-  # do I want to switch it to be a 1-1 mapping?
-  
-  # post-processing step to average both estimates?
-  
-  if(idx == 1){
-    L_final <- L1[,factors.idx.keep]
-  } else {
-    L_final <- L2[, factors.idx.keep]
+  return(list(L = L_final, similarity = cosine_sim_matrix))
+}
+
+stability_selection_row_split_post_processing <- function(L1, L2, F1, F2,
+                                                          threshold=0.99){
+  # use cosine similarity as similarity metric
+  # could try correlation but may run into issues if there is a constant baseline
+  n <- nrow(L1) + nrow(L2)
+
+  # compute cosine similarity
+  cosine_sim_matrix <- compute_cosine_sim_matrix(F1, F2)
+
+  #compute maximum similarity for each factor
+  idx <- which.min(dim(cosine_sim_matrix))
+
+  #pick factors to keep
+  max_similarity <- apply(cosine_sim_matrix, idx, max)
+  max_similarity_est2_idx <- apply(cosine_sim_matrix, idx, which.max)
+  factors.idx.keep <- which(max_similarity > threshold)
+
+  # option 1: concatenate the L estimates; doesn't feel the best?
+  if (length(factors.idx.keep) == 0){
+    L_final = matrix(0, nrow = n, ncol = 1)
+  } else if (idx == 1) {
+    print('idx = 1')
+    L_final <- rbind(L1[,factors.idx.keep, drop = FALSE], L2[,max_similarity_est2_idx[factors.idx.keep], drop = FALSE])
+  } else { # captures idx == 2
+    print('idx = 2')
+    L_final <- rbind(L1[,max_similarity_est2_idx[factors.idx.keep]], L2[, factors.idx.keep])
   }
+
+  # option 2: get an average estimate for F and then get an estimate for L using least squares or something
   return(list(L = L_final, similarity = cosine_sim_matrix))
 }
